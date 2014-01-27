2347008b87d19107f47f7931c33169d5ae635f26
Chritiano Anderson <anderson@nodeware.com.br>
2014-01-27 19:11:40 -0200

    Adicionado models TipoGeo

--- territorio/models.py
+++ territorio/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis import admin
 
-# Create your models here.
+
+class TipoGeo(models.Model):
+    '''Classe do TipoGeo'''
+
+    cod = models.IntegerField('Codigo TipoGeo')
+    nome = models.CharField('Nome TipoGeo', max_length = 100)
+
+    def __unicode__(self):
+        return self.nome 
+
+admin.site.register(TipoGeo)
