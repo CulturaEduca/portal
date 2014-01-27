80e1ddc0e8a712c5f61e4131fa6ba87de819fa88
Chritiano Anderson <anderson@nodeware.com.br>
2014-01-27 19:49:15 -0200

    Acerto na models do TipoGeo para pegar o cod como pk

--- territorio/models.py
+++ territorio/models.py
@@ -5,7 +5,7 @@ from django.contrib.gis import admin
 class TipoGeo(models.Model):
     '''Classe do TipoGeo'''
 
-    cod = models.IntegerField('Codigo TipoGeo')
+    cod = models.IntegerField('Codigo TipoGeo', unique=True, primary_key=True)
     nome = models.CharField('Nome TipoGeo', max_length = 100)
 
     def __unicode__(self):
